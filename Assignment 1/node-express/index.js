c312342e416c577cc831d91d4446316815d66454
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-03 14:59:06 +0500

    Assignment 1

--- Assignment 1/node-express/index.js
+++ Assignment 1/node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -12,6 +14,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
