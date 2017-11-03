3e4998e648fbee53a446f7404965b15f3d33d6d1
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-03 13:10:45 +0500

    Express Serve Static Files

--- Assignment 1/node-express/index.js
+++ Assignment 1/node-express/index.js
@@ -1,10 +1,17 @@
 const express = require('express'),
+
 http = require('http');
+
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 console.log(req.headers);
