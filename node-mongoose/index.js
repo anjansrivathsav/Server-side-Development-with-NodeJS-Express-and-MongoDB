859597f265bb38fc068cb7c30eac2ad7382d936d
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-04 13:47:39 +0500

    Mongoose Part 1

---
+++ node-mongoose/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url, {
+    useMongoClient: true
+});
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthappizza',
+        description: 'test'
+    });
+
+    newDish.save()
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({}).exec();
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return db.collection('dishes').drop();
+        })
+        .then(() => {
+            return db.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
---
+++ node-mongoose/models/dishes.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+
+module.exports = Dishes;
