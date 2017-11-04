28351539b39e3358717dedff35b010346a012b92
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-04 14:19:29 +0500

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -13,29 +13,39 @@ connect.then((db) => {
     console.log('Connected correctly to server');
 
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
+
         return Dishes.findByIdAndUpdate(dish._id, {
-                $set: {
-                    description: 'Updated Test'
-                }
-            }, {
-                new: true
-            })
-            .exec();
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
     .then((dish) => {
         console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
         return db.collection('dishes').drop();
     })
     .then(() => {
-        return db.close();        
+        return db.close();
     })
     .catch((err) => {
-
+        console.log(err);
     });
 });
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const dishSchema = new Schema({
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var dishSchema = new Schema({
     name: {
         type: String,
         required: true,
@@ -10,8 +29,9 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
-},{
+    },
+    comments:[commentSchema]
+}, {
     timestamps: true
 });
 
