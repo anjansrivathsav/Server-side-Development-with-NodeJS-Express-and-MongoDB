e3be705aa4fa88fadbba33c5ebc54a58e7a0b6b5
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-08 16:34:50 +0500

    File Upload

--- Assignment 4/conFusionServer/app.js
+++ Assignment 4/conFusionServer/app.js
@@ -12,6 +12,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 var config = require('./config');
 
 const mongoose = require('mongoose');
@@ -91,6 +92,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
