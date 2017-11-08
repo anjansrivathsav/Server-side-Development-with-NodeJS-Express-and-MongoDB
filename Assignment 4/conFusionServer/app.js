6aa10be8d1513c0c88bad45a4b0de300dcf95d3a
Zeeshan Haider <zeeshanhaider87@gmail.com>
2017-11-08 22:32:24 +0500

    Assignment 4

--- Assignment 4/conFusionServer/app.js
+++ Assignment 4/conFusionServer/app.js
@@ -13,6 +13,7 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter')
 var config = require('./config');
 
 const mongoose = require('mongoose');
@@ -93,6 +94,7 @@ app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favoriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
